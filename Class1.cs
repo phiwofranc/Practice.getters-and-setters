d769f37727b58cbda09c39fda7c55917a4df0ebf
phiwofranc <phiwofranc@gmail.com>
2023-05-14 09:52:44 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Movie
+    {
+        //public means it can be accesed by any part of the program
+        //private means only code within the class can access the property
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie (string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle; 
+            director = aDirector; 
+            Rating = aRating;
+        }
+
+        //propery is special method for getters and setters
+        //static attribute is an attribute that is shared by all the objects in that class, not just the object itself
+        public string Rating
+        {
+            get 
+            {
+                return rating; 
+            }
+            set 
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR") 
+                { 
+                    rating = value;
+                }
+                else 
+                { 
+                    rating = "NR"; 
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie Shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            avengers.Rating = "dog";
+            Console.WriteLine(avengers.Rating);
+        }
+    }
+}
